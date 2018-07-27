2a832a9f236ceda0fa5619c5f1b799101d3c3557
Maxwell Stern <maxwell.stern@makingwaves.com>
2018-07-27 17:01:46 -0500

    user middleware

--- warbler-server/models/user.js
+++ warbler-server/models/user.js
@@ -21,6 +21,29 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// hash password
+userSchema.pre('save', async function(){
+    try {
+        if(!this.isModified('password')) {
+            return next();
+        }
+        let hashPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashPassword;
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+userSchema.method.comparePassword = async function(candidatePassword, next) {
+    try {
+        let isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch(err){
+        return next(err);
+    }
+}
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
